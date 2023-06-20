6b1845c79028c8d1f286362ebfee8a9a97bb5556
thabani <kennyncwane@gmail.com>
2023-06-20 04:20:03 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet -Make the alphabet
+ * Return: void
+ */
+
+void print_alphabet(void)
+
+{
+	char c;
+	for (c = '0'; c <= 'Z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
