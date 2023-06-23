17cddf38d162d3ba3785d080b50d732911b7f279
thabani <kennyncwane@gmail.com>
2023-06-23 04:27:23 -0700

    Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - test functions for positive or negetive
+ * Retur: 0
+ */
+
+int main(void)
+{
+	int i;
+	i = 0;
+	positive_or_negetive(i);
+
+return (0);
+}
---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include <stdio>
+
+/**
+ * main - couses an infinit loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	printf("infinite loop incoming :(\n");
+
+	i = 0
+	
+	/*while (i < 10)*/
+	/*{*/
+	/*  putchar(i);*/
+	/*}*/
+
+	printf("infinite loop avoided! \\0/\n");
+
+	return (0);
+}
