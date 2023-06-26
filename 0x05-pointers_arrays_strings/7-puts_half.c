ea133773cf6e8e7a3b1b040e4b127715948ef250
thabani <kennyncwane@gmail.com>
2023-06-26 05:27:51 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_array - a function that prints n elements of an array
+ * @a: array name
+ * @n: is the number of elements OF the array to be printed
+ * Return: a and n inputs
+ */
+
+void print_array(int *a, int n)
+{
+	int a, n, longi;
+
+	longi = 0;
+
+	for (a = 0; str[a] != '\0'; a++)
+		longi++;
+
+	n = (longi /2);
+
+	if ((longi % 2) == 1)
+		n = ((longi + 1)/2);
+
+	for (a = n; str[a] != '\0'; a++)
+		_putchar(str[a]);
+	_putchar('\n')		
+}
